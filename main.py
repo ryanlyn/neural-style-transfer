bacb63a322fb11409524e7c150f0e73581850632
ryanlyn <mailboxryanlin@icloud.com>
2019-01-23 11:31:11 +1100

    add argparser and gui argparser

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+import argparse
 
 import torch
 import torchvision.models as models
+
+from gooey import Gooey, GooeyParser
 
 from model import run_style_transfer
 from loader import transform_image_tensors
@@ -32,14 +35,74 @@ def run(style_path, content_path):
     return output
 
 
-def run_and_export(style_path, content_path, output_path):
+def run_and_export(style_path, content_path, output_dir, output_name):
     output = run(style_path, content_path)
+    output_path = os.path.join(output_dir, output_name)
     save_image(output, output_path)
 
 
+@Gooey(program_name='Neural Style Transfer', required_cols=1, optional_cols=1, default_size=(680, 500))
+def parse_args_gui():
+    parser = GooeyParser(description='neural style transfer between style and content images')
+
+    parser.add_argument(
+        'style_path', type=str, widget='FileChooser', help='path to the style image'
+    )
+
+    parser.add_argument(
+        'content_path', type=str, widget='FileChooser', help='path to the content image'
+    )
+
+    parser.add_argument(
+        'output_dir', type=str, widget='DirChooser', help='directory in where output should be saved'
+    )
+
+    parser.add_argument(
+        'output_name', type=str, help='output filename'
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('neural style transfer between style and content images')
+
+    style_path_default = os.path.join('.', 'data', 'picasso.jpg')
+    parser.add_argument(
+        '--style_path', type=str, default=style_path_default, help='path to the style image'
+    )
+
+    content_path_default = os.path.join('.', 'data', 'dancing.jpg')
+    parser.add_argument(
+        '--content_path', type=str, default=content_path_default, help='path to the content image'
+    )
+
+    output_dir_default = os.path.join('.', 'output')
+    parser.add_argument(
+        '--output_dir', type=str, default=output_dir_default, help='directory in where output should be saved'
+    )
+
+    output_name_default = 'picasso_dancing.jpg'
+    parser.add_argument(
+        '--output_name', type=str, default=output_name_default, help='output filename'
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    run_and_export(
+        style_path=args.style_path,
+        content_path=args.content_path,
+        output_dir=args.output_dir,
+        output_name=args.output_name
+    )
+    return None
+
+
 if __name__ == '__main__':
-    style_image_path = os.path.join('.', 'data', 'picasso.jpg')
-    content_image_path = os.path.join('.', 'data', 'dancing.jpg')
-    output_image_path = os.path.join('.', 'output', 'picasso-dancing.jpg')
-    run_and_export(style_path=style_image_path, content_path=content_image_path, output_path=output_image_path)
+    arguments = parse_args_gui()
+    main(arguments)
 
